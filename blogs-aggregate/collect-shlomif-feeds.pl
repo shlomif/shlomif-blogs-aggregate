0bd581e961e8af0512638b2a9d1eb1191f3f4805
(no author) <(no author)@d85b70d9-4c6b-4e9e-bdf9-9f2cb0c2cca4>
2007-08-13 09:42:24 +0000

    Added the network bandwidth optimization in which the feed is only downloaded once. The script is now a bit quick and dirty but it works much better.

--- blogs-aggregate/collect-shlomif-feeds.pl
+++ blogs-aggregate/collect-shlomif-feeds.pl
@@ -3,7 +3,10 @@
 use strict;
 use warnings;
 
-my %feeds=
+use XML::Feed;
+use List::Util qw(min);
+
+my %feed_urls=
 (
     'homesite' => "http://community.livejournal.com/shlomif_hsite/data/rss",
     'tech' => "http://community.livejournal.com/shlomif_tech/data/rss",
@@ -32,36 +35,105 @@ my @collections =
     },  
 );
 
-sub get_feed_url_param
+sub get_feed
 {
-    my $id = shift;
+    my ($id, $url) = @_;
 
     # Enable to overcome the linmagazine invalid feed.
-    return () if ($id eq "linmag");
+    return if ($id eq "linmag");
 
-    my $f = $feeds{$id}; 
-    if (!defined($f))
+    my $url_feed = XML::Feed->parse(URI->new($url))
+        or die "For feed '$url' " . XML::Feed->errstr;
+
+    return $url_feed;
+}
+
+sub myconvert
+{
+    my ($feed, $output_format) = @_;
+    if (
+        (($output_format eq "RSS") && ($feed->format() eq "Atom")) ||
+        (($output_format eq "Atom") && ($feed->format() ne "Atom"))
+       )
     {
-        die "Unknown feed \"$f\"!";
+        return $feed->convert($output_format);
     }
-    return ("--url", $f);
+    else
+    {
+        return $feed;
+    }
+}
+
+my %feeds;
+
+while (my ($id, $url) = each(%feed_urls))
+{
+    $feeds{$id} = get_feed($id,$url);
 }
 
 foreach my $col (@collections)
 {
-    my @cmd = ("$ENV{HOME}/bin/xml-feed-collect",
-        (map { 
-                get_feed_url_param($_)
-            } @{$col->{feeds}}
-        ),
-        "-o", "to-upload/$col->{fn}.xml",
-        "--num-items=$col->{items}",
-        "--feed-link", "http://shlomif.livejournal.com/",
-    );
-    # print join(" ", (map {"'$_'" } @cmd)), "\n";
-    if (system(@cmd))
+    # Configuration for this feed.
+    my $output_format = "RSS";
+    my $num_items = $col->{items};
+    my $feed_link = "http://shlomif.livejournal.com/";
+    my $output_file = "to-upload/$col->{fn}.xml";
+
+    my $total_feed = XML::Feed->new($output_format) or
+        die XML::Feed->errstr;
+
+    foreach my $feed_id (@{$col->{feeds}})
     {
-        die "Could not run aggregator for $col->{fn}!";
+        if (defined($feeds{$feed_id}))
+        {
+            $total_feed->splice(myconvert($feeds{$feed_id}, $output_format));
+        }
+    }
+
+    my $feed_with_less_items = XML::Feed->new($output_format) or
+        die XML::Feed->errstr;
+
+    my @entries = $total_feed->entries();
+
+=begin Removed
+
+    @entries = 
+    (grep
+        {
+            (defined($subj_filter) ? ($_->title() =~ /$subj_filter/) : 1)
+                &&
+            (defined($subj_filter_out) ? ($_->title() !~ /$subj_filter_out/) : 1)
+        }
+        @entries
+    );
+
+=end
+
+=cut
+
+    @entries = (reverse(sort { $a->issued() <=> $b->issued() } @entries));
+
+    foreach my $e (@entries[0 .. min($num_items-1, $#entries)])
+    {
+        $feed_with_less_items->add_entry($e);
+    }
+
+    $feed_with_less_items->link($feed_link);
+
+    {
+        my $out;
+
+        if ($output_file)
+        {
+            open $out, ">", $output_file;
+        }
+        else
+        {
+            open $out, ">&STDOUT";
+        }
+        binmode $out, ":utf8";
+        print {$out} $feed_with_less_items->as_xml();
+        close($out);
     }
 }
 
