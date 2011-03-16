fe3416c352333f71a6935a92feb1d3bce5accd82
(no author) <(no author)@d85b70d9-4c6b-4e9e-bdf9-9f2cb0c2cca4>
2011-03-16 12:44:44 +0000

    Add more.

--- blogs-aggregate/upload.sh
+++ blogs-aggregate/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. "$HOME/conf/Bash/defs/homepage.bash"
+. "$HOME/conf/trunk/shlomif-settings/Bash/defs/homepage.bash"
 cd to-upload
 rsync --rsh=ssh *.xml "${__HOMEPAGE_REMOTE_PATH}/me/blogs/agg/"
 rsync --rsh=ssh *.xml "${HOMEPAGE_SSH_PATH}/me/blogs/agg/"
